8e98b79a2717cb01739cda85a23cd5608d63d434
Mery Chonishvili <mchon14@freeuni.edu.ge>
2022-07-06 23:52:16 +0400

    Add models for diagnoses, symptoms, focal elements, symptom weights
    Register models in admin

--- dempster_shafer_structure/admin.py
+++ dempster_shafer_structure/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from dempster_shafer_structure.models import Diagnose, Symptom, FocalElement, FocalElementSymptomWeight
+
+
+class DiagnoseAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Diagnose, DiagnoseAdmin)
+
+
+class SymptomAdmin(admin.ModelAdmin):
+    list_display = ['position', 'name']
+    ordering = ('position',)
+
+
+admin.site.register(Symptom, SymptomAdmin)
+
+
+class FocalElementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bpa', 'symptom_list')
+    ordering = ('name',)
+
+    def symptom_list(self, obj):
+        return ', '.join(s.name for s in obj.symptoms.all())
+
+
+admin.site.register(FocalElement, FocalElementAdmin)
+
+
+class FocalElementSymptomWeightAdmin(admin.ModelAdmin):
+    list_display = ('focal_element', 'symptom', 'weight')
+    ordering = ('focal_element', 'symptom__position')
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        form = super(FocalElementSymptomWeightAdmin, self).get_form(request, obj, change, **kwargs)
+        if obj:
+            form.base_fields['symptom'].queryset = obj.focal_element.symptoms
+        return form
+
+
+admin.site.register(FocalElementSymptomWeight, FocalElementSymptomWeightAdmin)
--- dempster_shafer_structure/models.py
+++ dempster_shafer_structure/models.py
@@ -1,3 +1,53 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class Diagnose(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=256)
+
+    class Meta:
+        db_table = 'diagnoses'
+
+    def __str__(self):
+        return self.name
+
+
+class Symptom(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=256)
+    position = models.IntegerField(null=False)
+
+    class Meta:
+        db_table = 'symptoms'
+
+    def __str__(self):
+        return self.name
+
+
+class FocalElement(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=64)
+    symptoms = models.ManyToManyField(Symptom, related_name='focal_elements')
+    bpa = models.DecimalField(null=False, decimal_places=3, max_digits=4)
+
+    class Meta:
+        db_table = 'focal_elements'
+
+    def __str__(self):
+        return self.name
+
+
+class FocalElementSymptomWeight(models.Model):
+    focal_element = models.ForeignKey(FocalElement, related_name='symptom_weights', on_delete=models.CASCADE)
+    symptom = models.ForeignKey(Symptom, related_name='focal_element_weights', on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.focal_element.name} - {self.symptom.name}"
+
+    def symptom_position(self):
+        return self.symptom.position
+
+    def clean(self):
+        if self.symptom not in self.focal_element.symptoms.all():
+            raise ValidationError({
+                'symptom': [f'Must be one of {[", ".join(s.name for s in self.focal_element.symptoms.all())]}']
+            })
