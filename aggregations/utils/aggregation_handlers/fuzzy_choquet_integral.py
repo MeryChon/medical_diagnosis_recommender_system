7e28191c1a9eda2de074954cfd5fa9892a6f5888
Mery Chonishvili <mchon14@freeuni.edu.ge>
2022-07-08 00:04:41 +0400

    Fix Choquet integral aggregation
    Use belief measure as a fuzzy measure in the aggregation

--- aggregations/utils/aggregation_handlers/fuzzy_choquet_integral.py
+++ aggregations/utils/aggregation_handlers/fuzzy_choquet_integral.py
@@ -1,7 +1,7 @@
 import math
-import random
 
 from aggregations.utils.aggregation_handlers.abstract import AbstractAggregator
+from dempster_shafer_structure.models import FocalElement
 from utility_matrix.utils.fuzzy_numbers.qrofn import QROFN
 
 
@@ -10,7 +10,7 @@ class FuzzyChoquetIntegralAggregator(AbstractAggregator):
         result = {}
         for alternative_key, focal_collections in self.utility_collections_matrix.items():
             for focal_elem_key, collection in focal_collections.items():
-                collection.sort()
+                collection.sort(key=lambda x: x.get('value'))
                 result.setdefault(alternative_key, {})[focal_elem_key] = self._calculate_choquet_integral(collection)
 
         return result
@@ -21,7 +21,7 @@ class FuzzyChoquetIntegralAggregator(AbstractAggregator):
         """
         result = QROFN()
         for i in range(0, len(collection)):
-            result += collection[i].multiply_by_const(
+            result += collection[i].get('value').multiply_by_const(
                 self._calculate_associated_probability(collection, i))
 
         return result
@@ -30,18 +30,26 @@ class FuzzyChoquetIntegralAggregator(AbstractAggregator):
         if index == 0:
             associated_probability = self._calculate_fuzzy_measure([collection[0]])
         else:
-            # TODO pass list of symptoms instead of their utility values
             associated_probability = self._calculate_fuzzy_measure(
-                [collection[:index]]) - self._calculate_fuzzy_measure(collection[:index - 1])
+                collection[:index]) - self._calculate_fuzzy_measure(collection[:index - 1])
 
         return math.fabs(associated_probability)
 
-    def _calculate_fuzzy_measure(self, symptom_set):
-        # TODO calculate fuzzy measure instead of hard-coding it
-        # tmp = {
-        #     ["Irritability", "Lack of Appetite", "Difficulty Moving", "Visual Hallucinations",
-        #      "Auditory Hallucinations"]: 1,
-        #     []: 0
-        # }
+    @staticmethod
+    def _calculate_fuzzy_measure(symptoms_subset):
+        """
+        We'll use belief measure as a fuzzy measure
+        """
+        result = 0
 
-        return random.uniform(0, 1)
+        current_subset_symptoms = [s.get('id') for s in symptoms_subset]
+        all_focal_elements = FocalElement.objects.all()
+
+        for focal_element in all_focal_elements:
+            fe_symptoms = focal_element.symptoms.all()
+            for fe_symptom in fe_symptoms:
+                if str(fe_symptom.id) in current_subset_symptoms:
+                    result += focal_element.bpa
+                    break
+
+        return result
