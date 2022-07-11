0edd0a597efde0c4f0e3485769d93964803d0fb1
Mery Chonishvili <mchon14@freeuni.edu.ge>
2022-07-11 08:04:03 +0400

    Implement aggregation method for QRungFuzzyDempstersExtremeExpectations

--- aggregations/utils/aggregation_handlers/qrf_dempster_extreme_expectations.py
+++ aggregations/utils/aggregation_handlers/qrf_dempster_extreme_expectations.py
@@ -3,4 +3,23 @@ from aggregations.utils.aggregation_handlers.abstract import AbstractAggregator
 
 class QRungFuzzyDempstersExtremeExpectations(AbstractAggregator):
     def aggregate(self):
-        pass
+        result = {}
+        for alternative_key, focal_collections in self.utility_collections_matrix.items():
+            result[alternative_key] = {}
+            for focal_elem_id, collection in focal_collections.items():
+                min_expected_value = self.get_minimum_expected_value(collection).get('value')
+                max_expected_value = self.get_maximum_expected_value(collection).get('value')
+                result[alternative_key][focal_elem_id] = (min_expected_value + max_expected_value).multiply_by_const(
+                    0.5)
+
+        return result
+
+    @staticmethod
+    def get_minimum_expected_value(collection):
+        sorted_collection = sorted(collection, key=lambda x: x.get('value'))
+        return sorted_collection[0]
+
+    @staticmethod
+    def get_maximum_expected_value(collection):
+        sorted_collection = sorted(collection, key=lambda x: x.get('value'), reverse=True)
+        return sorted_collection[0]
