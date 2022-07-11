828ee434ddc51932c25946f2f26c56e4721428c3
Mery Chonishvili <mchon14@freeuni.edu.ge>
2022-07-11 14:26:58 +0400

    Fix ordered weighted average aggregation

--- aggregations/utils/aggregation_handlers/qrfowa.py
+++ aggregations/utils/aggregation_handlers/qrfowa.py
@@ -8,11 +8,10 @@ class QRungFuzzyOrderedWeightedAverage(AbstractAggregator):
         for alternative_key, focal_collections in self.utility_collections_matrix.items():
             for focal_elem_key, collection in focal_collections.items():
                 aggregated_payoff = QROFN()
-                weight_vector = self.weight_vectors.get(focal_elem_key)
+                weight_vector = list(self.weight_vectors.get(focal_elem_key).values())
                 collection.sort(key=lambda x: x.get('value'))
-                for item in collection:
-                    symptom_id = item.get('id')
-                    weight = weight_vector.get(symptom_id)
+                for i, item in enumerate(collection):
+                    weight = weight_vector[i]
                     product = item.get('value').multiply_by_const(weight)
                     aggregated_payoff += product
                 result.setdefault(alternative_key, {})[focal_elem_key] = aggregated_payoff
